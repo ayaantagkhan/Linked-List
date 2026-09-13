d584c6a6793b7e111c4f582da1fe5918d2ace632
ayaantagkhan <a.khan021004@gmail.com>
2026-09-12 21:58:27 -0500

    Made Linked List more modular with a main file

--- reversal.py
+++ reversal.py
@@ -1,21 +1,14 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+from linkelist import Node, LinkedList
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
+def Reversal(self):
+    previous = None
+    current = self.head
 
-    def Reversal(self):
-        previous = None
-        current = self.head
-
-        while current:
-            next_node = current.next
-            current.next = previous
-            previous = current 
-            current = next_node 
+    while current:
+        next_node = current.next
+        current.next = previous
+        previous = current 
+        current = next_node 
 
         self.head = previous 
 
